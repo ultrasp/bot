cbc98682e44a8f972f729dbac8681bcbbee3871a
Umidjon Hamidov <samirchik89@gmail.com>
2023-05-14 16:32:19 +0500

    send message

---
+++ app/Console/Commands/SendMessages.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\MessageSending;
+use Illuminate\Console\Command;
+
+class SendMessages extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'send:message';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        if( date( 'H') == 0 && date( 'i') == 0) {
+            MessageSending::createSendings();
+        }
+        MessageSending::send();
+    }
+}
