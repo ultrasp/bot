abccd487c81447410e4257cae6d008c2b0baeb42
Umidjon Hamidov <samirchik89@gmail.com>
2023-05-14 16:39:27 +0500

    add read templates

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\MessageSending;
+use App\Services\GoogleService;
 
 class CronController extends Controller
 {
     public function run(){
         if( date( 'H') == 0 && date( 'i') == 0) {
             MessageSending::createSendings();
+        }
+        if(date( 'i') == 0){
+            $service = new GoogleService();
+            $service->readValues();
         }
         MessageSending::send();
     }
