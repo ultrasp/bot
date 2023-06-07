5d2d7d5420dbb14789c7438379987d90b8c6d5d8
Umidjon Hamidov <samirchik89@gmail.com>
2023-06-07 15:54:50 +0500

    fix bug

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -56,7 +56,7 @@ class TelegramController extends Controller
     public function handleManagerMessage($data = null)
     {
         // try {
-        $data = '{"update_id":736985814,"message":{"message_id":2,"from":{"id":2242981,"is_bot":false,"first_name":"Umid","last_name":"Hamidov","username":"Samirchik03","language_code":"en"},"chat":{"id":-1001524098666,"first_name":"Umid","last_name":"Hamidov","username":"Samirchik03","type":"private"},"date":1685029690,"text":"\/officeoff","entities":[{"offset":0,"length":9,"type":"bot_command"}]}}';
+        // $data = '{"update_id":736985814,"message":{"message_id":2,"from":{"id":2242981,"is_bot":false,"first_name":"Umid","last_name":"Hamidov","username":"Samirchik03","language_code":"en"},"chat":{"id":-1001524098666,"first_name":"Umid","last_name":"Hamidov","username":"Samirchik03","type":"private"},"date":1685029690,"text":"\/officeoff","entities":[{"offset":0,"length":9,"type":"bot_command"}]}}';
         $message = json_decode($data);
         $service = new TelegramService();
         // dd($message);
