a4c98538883148b835b5f2486bd3d2c9aa4f8170
Umidjon Hamidov <samirchik89@gmail.com>
2023-06-11 09:01:41 +0500

    fix

--- app/Managers/TelegramManager.php
+++ app/Managers/TelegramManager.php
@@ -194,7 +194,7 @@ class TelegramManager
     public function forwardMessage($tUpdate = null)
     {
         // $tUpdate = '{"update_id":319791824,"message":{"message_id":2799,"from":{"id":2242981,"is_bot":false,"first_name":"Umid","last_name":"Hamidov","username":"Samirchik03","language_code":"en"},"chat":{"id":2242981,"first_name":"Umid","last_name":"Hamidov","username":"Samirchik03","type":"private"},"date":1686455277,"photo":[{"file_id":"AgACAgIAAxkBAAIK72SFQ-wJEdPZj9HsnH8AAXuZ8Oqa-QACm8wxG0dqKUgQqRxybRKa-wEAAwIAA3MAAy8E","file_unique_id":"AQADm8wxG0dqKUh4","file_size":914,"width":90,"height":51},{"file_id":"AgACAgIAAxkBAAIK72SFQ-wJEdPZj9HsnH8AAXuZ8Oqa-QACm8wxG0dqKUgQqRxybRKa-wEAAwIAA20AAy8E","file_unique_id":"AQADm8wxG0dqKUhy","file_size":11255,"width":320,"height":180},{"file_id":"AgACAgIAAxkBAAIK72SFQ-wJEdPZj9HsnH8AAXuZ8Oqa-QACm8wxG0dqKUgQqRxybRKa-wEAAwIAA3gAAy8E","file_unique_id":"AQADm8wxG0dqKUh9","file_size":50145,"width":800,"height":450},{"file_id":"AgACAgIAAxkBAAIK72SFQ-wJEdPZj9HsnH8AAXuZ8Oqa-QACm8wxG0dqKUgQqRxybRKa-wEAAwIAA3kAAy8E","file_unique_id":"AQADm8wxG0dqKUh-","file_size":97504,"width":1280,"height":720}]}}';
-        $tUpdate = json_decode($tUpdate);
+        // $tUpdate = json_decode($tUpdate);
         $tgService = new TelegramService();
         // $tgService->sendBotId = TelegramService::MANAGER_BOT_ID;
         $responce = $tgService->forwardMessage(TelegramService::MANAGER_GROUP_ID, $tUpdate->message->chat->id, $tUpdate->message->message_id);
